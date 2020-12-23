1bc76992cdf1cc9b0a941cd692ad23fc30fe4eec
wonAdam <ehdgus5500@gmail.com>
2020-12-24 00:38:50 +0900

    init

---
+++ baekjoon/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
